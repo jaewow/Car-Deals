936499f8e0e52f83c89e366437f915382f6cade8
Jae Lim <dylanjaelim@gmail.com>
2019-05-27 03:32:30 -0500

    Need to inspect elements

--- CarScraper.py
+++ CarScraper.py
@@ -5,6 +5,7 @@ if __name__ == "__main__":
 	#Insert code for main method
 	page = requests.get("https://www.autotrader.com/cars-for-sale/searchresults.xhtml?zip=75033&makeCodeList=HYUND&modelCodeList=ELANTR")
 	tree = html.fromstring(page.content)
+	print(tree)
 else:
 	#Insert code if not being run as the main program
 	print("not main method")
