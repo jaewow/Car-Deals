36b8ce4a38ba52a1beb3b8b1880e22d353fd083c
Jae Lim <dylanjaelim@gmail.com>
2019-05-28 01:18:29 -0500

    Currently does not print anything

--- CarScraper.py
+++ CarScraper.py
@@ -5,7 +5,8 @@ if __name__ == "__main__":
 	#Insert code for main method
 	page = requests.get("https://www.autotrader.com/cars-for-sale/searchresults.xhtml?zip=75033&makeCodeList=HYUND&modelCodeList=ELANTR")
 	tree = html.fromstring(page.content)
-	print(tree)
+	script = tree.xpath('//*[@id="mountNode"]/div/div[3]/div/div[2]/div[1]/div[2]/div[3]/div/div[2]/script[4]/text()')
+	print(script)
 else:
 	#Insert code if not being run as the main program
 	print("not main method")
